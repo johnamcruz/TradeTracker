5036c501c3f6bcc5e6fb8571d77a9031e067b032
John Cruz <johnamcruz@gmail.com>
2024-10-09 09:53:15 -1000

    updated

---
+++ Source/TradeTracker/TradeTracker/Views/ImportView.swift
@@ -0,0 +1,18 @@
+//
+//  ImportView.swift
+//  TradeTracker
+//
+//  Created by John M Cruz on 10/9/24.
+//
+
+import SwiftUI
+
+struct ImportView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImportView()
+}
