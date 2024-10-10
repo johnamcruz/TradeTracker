9ae500a761a3fec9d22f7695a662d5e8938f88b9
John Cruz <johnamcruz@gmail.com>
2024-10-09 19:53:39 -1000

    updated trade model:

--- Source/TradeTracker/TradeTracker/Models/Trade.swift
+++ Source/TradeTracker/TradeTracker/Models/Trade.swift
@@ -7,5 +7,17 @@
 import Foundation
 
 struct Trade: Codable {
+    var instrument: String
+    var description: String
+    var transactions: [Transaction]
+    var totalQuantity: Int
+    var totalAmount: Double
+    var totalBought: Double
+    var totalSold: Double
+    var totalBoughtQty: Int
+    var totalSoldQty: Int
     
+    var profitOrLoss: Double {
+        totalSold - totalBought
+    }
 }
