2e53225f1bde38ade5fd9c24e5638eb896dd63c9
John Cruz <johnamcruz@gmail.com>
2024-10-09 17:21:15 -1000

    updated objects

---
+++ Source/TradeTracker/TradeTracker/Models/Trade.swift
@@ -0,0 +1,11 @@
+//
+//  Trade.swift
+//  TradeTracker
+//
+//  Created by John M Cruz on 10/9/24.
+//
+import Foundation
+
+struct Trade: Codable {
+    
+}
---
+++ Source/TradeTracker/TradeTracker/Models/Transaction.swift
@@ -0,0 +1,12 @@
+//
+//  Transaction.swift
+//  TradeTracker
+//
+//  Created by John M Cruz on 10/9/24.
+//
+
+import Foundation
+
+struct Transaction: Codable {
+    
+}
