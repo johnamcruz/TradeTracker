dc111c2a214103149645decd374b21556991d587
John Cruz <johnamcruz@gmail.com>
2024-10-12 22:51:53 -1000

    updated format

--- Source/TradeTracker/TradeTracker/Views/TradeView.swift
+++ Source/TradeTracker/TradeTracker/Views/TradeView.swift
@@ -12,17 +12,19 @@ struct TradeView: View {
     
     var body: some View {
         HStack(spacing: 0) {
-            VStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: 0) {
                 HStack {
                     Text(viewModel.ticker)
-                    Text("\(viewModel.strikePrice)")
+                    Text("\(viewModel.strikePrice, specifier: "%.0f")")
                     Text(viewModel.optionType.rawValue)
                 }
+                Text(viewModel.activityDate.formatted(date: .abbreviated, time: .omitted))
             }
             Spacer()
-            Text("\(viewModel.profitOrLoss)")
+            Text("\(viewModel.profitOrLoss, specifier: "%.2f")")
                 .font(.caption)
         }
+        .padding()
     }
 }
 
